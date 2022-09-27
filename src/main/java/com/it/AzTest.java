8e6f0bb00d6740cdd5874f9d4d9967a2d6272f34
xiaofang <1961306389@qq.com>
2022-09-27 19:14:06 +0800

    Initial commit

---
+++ src/main/java/com/it/AzTest.java
@@ -0,0 +1,16 @@
+package com.it;
+
+/**
+ * @author ZuYingFang
+ * @time 2022-04-22 17:12
+ * @description
+ */
+public class AzTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
